4f76941660d07d9382d50d8d627f76bd3b3ccc11
BuildTools <unconfigured@null.spigotmc.org>
2024-08-24 23:11:55 +0800

    update example

--- scripts/example.jsm.kts
+++ scripts/example.jsm.kts
@@ -1,9 +1,16 @@
+import me.hellrevenger.generated.*
+import me.hellrevenger.library.api.EventListener
 import xyz.wagyourtail.jsmacros.client.api.event.impl.world.EventTick
 import xyz.wagyourtail.jsmacros.core.service.EventService
-JsMacros.on("Tick", JavaWrapper.methodToJava(fun(e: EventTick, _: Any) {
+
+EventListener(context, EventTick::class.java, {
     val time = World.time
     Chat.actionbar("time: $time")
-} as Function2<*,*,*>))
+})
+
+fun getFishHook() = Client.minecraft.player?.fishHook
+
+getFishHook()?.getHookedEntity()
 
 (event as EventService).stopListener = JavaWrapper.methodToJava(fun(){
 
