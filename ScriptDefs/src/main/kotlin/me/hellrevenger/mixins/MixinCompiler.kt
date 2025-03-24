0dad61cda7b09ad26da07ce6557fc6981de513f9
reply7251 <zxc989876@gmail.com>
2025-03-25 03:44:12 +0800

    wtf config

--- ScriptDefs/src/main/kotlin/me/hellrevenger/mixins/MixinCompiler.kt
+++ ScriptDefs/src/main/kotlin/me/hellrevenger/mixins/MixinCompiler.kt
@@ -1,11 +1,13 @@
 package me.hellrevenger.mixins
 
+import com.google.gson.JsonObject
+import me.hellrevenger.SharedLibraries
 import me.hellrevenger.library.api.RuntimeMixin
 import net.bytebuddy.description.method.MethodDescription
 import net.bytebuddy.implementation.MethodDelegation
 import net.bytebuddy.matcher.ElementMatchers
-import xyz.wagyourtail.jsmacros.client.api.library.impl.FChat
 import xyz.wagyourtail.jsmacros.core.Core
+import xyz.wagyourtail.jsmacros.core.config.ConfigManager
 import xyz.wagyourtail.jsmacros.core.config.Option
 
 class MixinCompiler {
@@ -13,19 +15,33 @@ class MixinCompiler {
         val clazz = Class.forName("org.jetbrains.kotlin.scripting.compiler.plugin.impl.ScriptJvmCompilerImplsKt")
         val matcher = ElementMatchers.named<MethodDescription>("doCompileWithK2")
         val delegate = MethodDelegation.withDefaultConfiguration().filter(ElementMatchers.named("doCompile")).to(clazz)
-        val config = Core.getInstance().config
+        var config: ConfigManager? = null
 
-        fun mixin() {
-            config.addOptions("MixinCompiler", MixinCompiler::class.java)
-            config.getOptions(MixinCompiler::class.java).let {
-                if(!it.K2Enabled) {
-                    it.onDisabled()
-                }
-            }
+        fun mixin(runner: Core<*, *>) {
+            config = runner.config
+            runner.config.addOptions("MixinCompiler", MixinCompiler::class.java)
+            isK2Enabled()
         }
 
-        fun isK2Enabled() = config.getOptions(MixinCompiler::class.java)?.K2Enabled ?: true
+        fun isK2Enabled(): Boolean {
+            config?.getOptions(MixinCompiler::class.java)?.let {
+                if(it.wasEnabled != it.K2Enabled) {
+                    it.wasEnabled = it.K2Enabled
+                    SharedLibraries.Chat.log("K2 enabled: ${it.K2Enabled}")
+                    if(it.K2Enabled) {
+                        it.onEnabled()
+                    } else {
+                        it.onDisabled()
+                    }
+                }
+                return it.K2Enabled
+            }
+            return true
+        }
     }
+
+    @Transient
+    var wasEnabled = true
 
     @JvmField
     @Option(translationKey = "K2", group = ["jsmacros.settings.general"], setter = "setEnabled")
@@ -34,9 +50,10 @@ class MixinCompiler {
     @JvmName("setEnabled")
     fun setEnabled(enabled: Boolean) {
         val flag = K2Enabled != enabled
+        wasEnabled = K2Enabled
         K2Enabled = enabled
         if(flag) {
-            FChat().log("K2 enabled: $K2Enabled")
+            SharedLibraries.Chat.log("K2 enabled: $K2Enabled")
             if(K2Enabled) {
                 onEnabled()
             } else {
@@ -53,5 +70,10 @@ class MixinCompiler {
     fun onDisabled() {
         RuntimeMixin.setIntercept(clazz, matcher, delegate)
         RuntimeMixin.doMixin(clazz)
+    }
+
+    @JvmName("fromV3")
+    fun fromV3(v3: JsonObject) {
+        setEnabled(v3.get("MixinCompiler").asJsonObject.get("K2Enabled").asBoolean)
     }
 }
